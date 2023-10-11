66f9842eeacc952a1e32fc259ac04cb659eaba7f
Retypolkg <retypolkg2021@gmail.com>
2023-10-11 19:36:03 +0200

    X2: Time for tequila!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X2.mk
+    $(LOCAL_DIR)/tequila_X2.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X2-userdebug \
-    lineage_X2-eng
+    tequila_X2-userdebug \
+    tequila_X2-eng
